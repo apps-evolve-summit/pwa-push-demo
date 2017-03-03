2d9760d9d0425599e44dd349be455bf0d9853fc0
M.Suska <msuska@gmail.com>
2017-03-03 09:54:22 +0100

    Changed message only for test

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('This is a great test!');
+    res.end('This is a great test after removing unused files!');
     
 }).listen(process.env.PORT || 8080);
